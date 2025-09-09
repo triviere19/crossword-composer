d4c03d29ffca0761bf814dfb1bff1304199ff737
triviere19 <triviere19@comcast.net>
2025-09-08 23:42:12 -0400

    remove ui

--- ui/src/main.js
+++
@@ -1,6 +0,0 @@
-import Crossword from './Crossword.svelte';
-
-new Crossword({
-	target: document.getElementById('app-root'),
-	props: {}
-});
--- ui/src/worker.js
+++
@@ -1,9 +0,0 @@
-import init, * as cw from '../../pkg/crossword';
-
-self.onmessage = ({data}) => {
-    init(data.wasm).then(() => {
-        let solver = cw.Solver.new(data.wordlist)
-        let result = solver.solve(data.grid)
-        postMessage(result)
-    })
-}
