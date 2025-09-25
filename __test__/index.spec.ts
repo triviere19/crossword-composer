c3ad40973a20b38de76d450d8c522bb3d8b8b501
triviere19 <triviere19@comcast.net>
2025-09-24 20:51:22 -0400

    update ts test with proper input grid

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,24 +2,53 @@ import test from 'ava'
 
 import { Solver } from '../index'
 
-test('sync function from native code', (t) => {
-    const words = ['CAT', 'DOG', 'BIRD', 'FISH', 'CAR', 'ART', 'TAR']
-    const solver = new Solver(words);
+test('Solver constructor creates instance', (t) => {
+	const words = ['CAT', 'DOG', 'BIRD', 'FISH', 'CAR', 'ART', 'TAR']
+	const solver = new Solver(words);
+	t.truthy(solver);
+	t.true(solver instanceof Solver);
+});
 
-    // Simple 3x3 grid specification
-    const grid = [
-      [1, 1, 1],
-      [1, 0, 1], 
-      [1, 1, 1]
-    ];
+test('Solver solves simple puzzle', (t) => {
+	const words = ['CAT', 'DOG', 'BIRD', 'FISH', 'CAR', 'ART', 'TAR', 'RAT', 'TAT'];
+	const solver = new Solver(words);
 
-    const result = solver.solve(grid);
+	// We expect to build the following puzzle:
+	// C A T
+	// A   A
+	// R A T
 
-    // Should return either a solution or null
-    if (result !== null) {
-      t.true(Array.isArray(result))
-      t.true(result.every(item => typeof item === 'string'))
-    } else {
-      t.is(result, null)
-    }
+	// This builds the following grid:
+	// 0 1 2
+	// 3   4
+	// 5 6 7
+
+	// And then given the grid, we want the following words:
+	const grid = [
+		[0, 3, 5,], 	// CAR
+		[2, 4, 7,], 	// TAR
+		[0, 1, 2,],     // CAT
+		[5, 6, 7,],     // RAT
+	];
+
+	const result = solver.solve(grid);
+
+	// Should return this solution
+	const expectedResult = [
+		'c', 'a', 't',
+		'a', 'a', 'r',
+		'a', 't'
+	];
+
+	console.log(result);
+
+	if (result !== null) {
+		t.true(Array.isArray(result));
+		t.true(result.every(item => typeof item === 'string'));
+		result.forEach((slot, i) => {
+			t.true(slot == expectedResult[i]);
+		})
+	} else {
+		t.fail();
+	}
 });
